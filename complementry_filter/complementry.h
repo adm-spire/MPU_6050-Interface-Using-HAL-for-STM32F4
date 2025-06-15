b7f68d16a84b92d05a46384fe452ae4d96e2c032
raunaq <raunaq2001singh@gmail.com>
2025-06-15 22:49:08 +0530

    complementry filter

---
+++ complementry_filter/complementry.h
@@ -0,0 +1,11 @@
+#ifndef INC_COMPLEMENTRY_H_
+#define INC_COMPLEMENTRY_H_
+
+
+void complementary_filter(float ax, float ay, float az, float gx, float gy, float dt);
+float get_roll();
+float get_pitch();
+
+
+
+#endif /* INC_COMPLEMENTRY_H_ */
---
+++ complementry_filter/mpu.h
@@ -0,0 +1,12 @@
+#ifndef INC_MPU_H_
+#define INC_MPU_H_
+
+void mpu_init(void);
+void MPU_ReadAccelGyro(int16_t* ax, int16_t* ay, int16_t* az,
+                       int16_t* gx, int16_t* gy, int16_t* gz);
+
+extern I2C_HandleTypeDef hi2c1;
+
+extern UART_HandleTypeDef huart2;
+
+#endif /* INC_MPU_H_ */
